c57bb5154521e86d5a6e63d940acbbeac449e749
José <joseantoniovf.cmb@gmail.com>
2024-06-26 08:49:00 -0430

    mitad de la programacion

--- main.cpp
+++ main.cpp
@@ -1,7 +1,42 @@
 #include <iostream>
-
+using namespace std;
 /* José Vásquez, Luis Espinoza, Adrian Aguirre, Jesus Arrieche */
 
-int main(int argc, char** argv) {
-	return 0;
+int main() {
+string razonSocial;
+int rifCedula;
+int cantidadCompras = 0;
+int MayorCantidadCompras = 0;
+float totalVentas = 0;
+
+cout, "Ingrese la razon social del cliente: ";		
+
+getline(cin,razonSocial);
+ std::cout, "ingrese rif/cedula del cliente: ";
+std::cin >> rifCedula;
+
+while 
+(razonSocial !="0" && rifCedula !=0) {
+	
+	int cantidadproductos;
+	float PrecioProducto;
+	float montoPagar;
+	
+	std::cout << "Ingrese la cantidad de productos comprados:";
+	std::cin >> cantidadproductos;
+	std::cout << "Ingrese nombre del producto:";
+	std::cin >> PrecioProducto;
+	
+	
+	montoPagar = cantidadproductos*PrecioProducto;
+	totalVentas +=montoPagar;
+	cantidadCompras++;
+	
+	if ( cantidadproductos > MayorCantidadCompras){
+		MayorCantidadCompras=cantidadproductos;
+	}
+	
 }
+}
+	
+	
